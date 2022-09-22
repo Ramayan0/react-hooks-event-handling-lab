eaaf54b8c4794382c7ee243052c1b188f3f5c9f2
hamsa <hafieekhan@gmail.com>
2022-09-22 11:54:43 +0300

    "Completed Lab"

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyeOnMe() {
+  function handleFocuses() {
+    console.log("Good!");
+  }
+  function handleBlaring() {
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+    <div>
+      <button onFocus={handleFocuses} onBlur={handleBlaring}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyeOnMe;
